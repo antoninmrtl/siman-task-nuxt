b14fa211b70970eb953a398a11196ac033c3dee5
amartel2025 <antonin.mrtl.pr@gmail.com>
2026-04-13 12:19:11 +0200

    feat: allow editing task text and  task priority on details page

--- pages/taches/[id].vue
+++ pages/taches/[id].vue
@@ -1,18 +1,34 @@
 <script setup>
 const route = useRoute()
 const tacheId = route.params.id
+const itemsSelect = ref(['Basse', 'Normal', 'Haute'])
+const value = ref('Basse')
 const taches = useState('taches-globales', () => [])
 const laTache = computed(() => taches.value[route.params.id])
 </script>
 
 <template>
   <UContainer class="py-10">
-    <UCard>
+    <UCard v-if="laTache">
       <h1>Détails de la tâche : {{ tacheId }}</h1>
       <div>
-        <h2>{{ laTache ? laTache.texte : 'Tâche introuvable' }}</h2>
+        <UFormGroup label="Modifier la tâche" class="mb-4 mt-3">
+
+          <div class="flex justify-center gap-5">
+            <UInput class="w-full" v-model="laTache.texte"/>
+            <USelect class="w-full" v-model="laTache.priorite" :options="itemsSelect"/>
+          </div>
+        </UFormGroup>
+
+        <div class="flex gap-5 p-5">
+          <h2>{{ laTache ? laTache.texte : 'Tâche introuvable' }}</h2>
+          <UBadge
+              :class="{'bg-red-500 text-white' : laTache.priorite == 'Haute', 'bg-blue-500' : laTache.priorite == 'Normal'}">
+            {{ laTache.priorite }}
+          </UBadge>
+        </div>
       </div>
-      <UButton to="/" variant="link">Retour à la liste</UButton>
+      <UButton to="/" variant="link">Sauvegarder</UButton>
     </UCard>
   </UContainer>
 </template>
